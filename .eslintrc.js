6b273d82290ae4bc8f71aaf69d15a28239f177ab
Rahim Rahman <rahim@r2integration.com>
2024-01-26 06:51:17 -0700

    chore: eslint plugins and setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: "@typescript-eslint/parser",
+  plugins: ["prettier", "@typescript-eslint"],
+  extends: [
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommeded",
+    "plugin:react/recommended"
+  ],
 };
